fc106c41ff59958ad5b34958705186e21559862f
Isis Tejeda <isis@isistejeda.com>
2017-04-26 14:18:15 -0500

    added some alignment to the views

--- app/controllers/activity_log_controller.rb
+++ app/controllers/activity_log_controller.rb
@@ -8,13 +8,20 @@ class ActivityLogController < ApplicationController
       set_habit
       @habit.activity_logs.create(params[:activity_log])
       redirect to "/habits/#{@habit.id}"
-
     else
       redirect to '/login'
     end
   end
 
+  get '/activity/:id//edit' do
 
+    if logged_in?
+      @activity = ActivityLog.find(params[:id])
+      erb :'activity_logs/edit'
+    else
+      redirect to '/login'
+    end
+  end
 
 
 end
