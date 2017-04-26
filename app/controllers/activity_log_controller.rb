05911bd8ee43bd322cd18ffc8f5634b4d24328b2
Isis Tejeda <isis@isistejeda.com>
2017-04-26 11:14:42 -0500

    stuck at checkbox while setting value = true

--- app/controllers/activity_log_controller.rb
+++ app/controllers/activity_log_controller.rb
@@ -1,4 +1,4 @@
-class ActivityController < ApplicationController
+class ActivityLogController < ApplicationController
   def set_habit
     @habit = Habit.find_by_id(params[:id])
   end
@@ -6,11 +6,21 @@ class ActivityController < ApplicationController
   post '/habits/:id/activity' do
     if logged_in?
       set_habit
+      @habit.activity_logs.create(params[:activity_log])
       redirect to "/habits/#{@habit.id}"
 
     else
       redirect to '/login'
     end
+  end
+
+
+  delete '/habits/:id/delete' do
+    set_habit
+    if @habit.user == current_user
+      @habit.delete
+    end
+    redirect to '/habits'
   end
 
 
