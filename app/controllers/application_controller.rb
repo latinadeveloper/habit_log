656ceaafaee4ddd127b31de85705e439d758062f
Isis Tejeda <isis@isistejeda.com>
2017-04-26 20:36:27 -0500

    before slugging

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,27 @@
+class ApplicationController < Sinatra::Base
+  def set_habit
+    @habit = Habit.find_by_id(params[:id])
+  end
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "habit"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:id]
+    end
+    def current_user
+      @current_user ||= User.find(session[:id])
+    end
+  end
+
+  get '/' do
+    erb :'/index'
+  end
+
+
+end
---
+++ app/controllers/habit_controller.rb
@@ -0,0 +1,69 @@
+class HabitController < ApplicationController
+  def set_habit
+    @habit = Habit.find_by_id(params[:id])
+  end
+
+  get '/habits' do
+    if logged_in?
+      @habits = Habit.all
+      erb :'habits/index'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/habits/new' do
+    if logged_in?
+      erb :'habits/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/habits' do
+    habit = current_user.habits.new(params[:habit])
+    if habit.save
+      redirect to '/habits'
+    else
+      redirect to '/habits/new'
+    end
+  end
+
+  #Show
+  get '/habits/:id' do
+    if logged_in?
+      set_habit
+      erb :'habits/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/habits/:id/edit' do
+    if logged_in?
+      set_habit
+      erb :'habits/edit'
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/habits/:id' do
+    set_habit
+    if @habit.update(params[:habit])
+      redirect to "/habits/#{@habit.id}"
+    else
+      redirect to "/habits/#{@habit.id}/edit"
+    end
+  end
+
+  delete '/habits/:id/delete' do
+    set_habit
+    if @habit.user == current_user
+      @habit.delete
+    end
+    redirect to '/habits'
+  end
+
+
+end
