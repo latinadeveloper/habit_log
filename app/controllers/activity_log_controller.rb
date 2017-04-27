baa7327d376c5bdb34c58011c42e863f7ce8cd63
Isis Tejeda <isis@isistejeda.com>
2017-04-26 20:33:36 -0500

    edit activity entry done

--- app/controllers/activity_log_controller.rb
+++ app/controllers/activity_log_controller.rb
@@ -2,7 +2,7 @@ class ActivityLogController < ApplicationController
   def set_habit
     @habit = Habit.find_by_id(params[:id])
   end
-
+  #creates new activity
   post '/habits/:id/activity' do
     if logged_in?
       set_habit
@@ -13,6 +13,7 @@ class ActivityLogController < ApplicationController
     end
   end
 
+  #form to edit activity
   get '/activity/:id/edit' do
 
     if logged_in?
@@ -20,6 +21,16 @@ class ActivityLogController < ApplicationController
       erb :'activity_logs/edit'
     else
       redirect to '/login'
+    end
+  end
+
+  #performs update
+  patch '/activity/:id' do
+    @activity = ActivityLog.find(params[:id])
+    if @activity.update(params[:activity])
+      redirect to "/habits/#{@activity.habit.id}"
+    else
+      redirect to "/activity/#{@activity.id}/edit"
     end
   end
 
